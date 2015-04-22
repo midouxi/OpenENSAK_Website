b95385bcb091a717fd3367d64bb5c6eae64ba449
MacBookPro <macbookpro@MacBook-Air-de-MacBookPro.local>
2015-04-22 13:51:28 +0100

    upload first time

---
+++ map/My-Map.js
@@ -0,0 +1,19 @@
+ $(function(){
+            //default
+            $('.map-canvas').prettyMaps({
+                address: 'Khouribga, Morocco',
+                    image: 'map-icon.png',
+                    hue: '#0073FF',
+                    saturation: -30,
+                    //zoom: 16,
+                    panControl: true,
+                    zoomControl: true,
+                    mapTypeControl: true,
+                    scaleControl: true,
+                    streetViewControl: true,
+                    overviewMapControl: true,
+                    scrollwheel: false,
+            });
+
+            
+        });
